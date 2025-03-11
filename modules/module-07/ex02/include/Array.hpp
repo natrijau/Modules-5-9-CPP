8c02e081c61700654eb4682318534a9b214ca27a
natrijau <>
2025-03-11 13:12:13 +0100

    ex02 module 07 a verifier

--- modules/module-07/ex02/include/Array.hpp
+++ modules/module-07/ex02/include/Array.hpp
@@ -2,6 +2,7 @@
 #define	ARRAY_HPP
 
 #include <iostream>
+#include <exception>
 
 template <typename T>
 class Array
@@ -13,24 +14,59 @@ class Array
 	
 	public:
 
-		Array() : _array(NULL){};
-		Array(unsigned int size) : _array(size) , _size(size){};
-		Array(const Array &copy)	:	_array(copy._array), _size(copy._size) 
-		{
-			for (size_t i = 0; i < this->_size; i++)
-				this->_array|[i] = copy._array[i];
-		};
-		~Array();
+		Array() : _array(NULL) , _size(0){};
 
-		const &operator=(const Array &other)
+		Array(unsigned int n) : _size(n){
+			this->_array = new T[n];
+		};
+
+		Array(const Array &copy) :	 _size(copy._size) 
 		{
+			this->_array = new T[copy._size];
+			for (size_t i = 0; i < this->_size; i++)
+			{
+				this->_array[i] = copy._array[i];
+			};
+		};
+
+		~Array(){
+			if (this->_array.size() == 1)
+				delete this->_array;
+			else
+				delete[] this->_array;		
+		};
+
+		class	OutBounds : public	std::exception{
+			virtual const char* what() const throw()
+			{
+				return ("dasdsadas");
+			};
+		};
+
+		Array &operator=(const Array &other)
+		{
+			delete[] this->_array;
 			if (this != other)
 			{
+				this->_array = new T[other._size];
 				this->_size = other._size;
 				for (size_t i = 0; i < this->_size; i++)
-					this->_array|[i] = copy._array[i];
+				{
+					this->_array[i] = other._array[i];
+				}
 			}
-			return (this);
+			return (*this);
+		};
+
+		const	T	&operator[](int idx) const{
+			if(idx <= 0 || idx > this->_size){
+				throw	OutBounds();
+			}
+    	    return (this->_array[idx]);
+    	};
+
+		unsigned int	size() const{
+			return (this->_size);
 		};
 };
 //std::ostream &operator<<(std::ostream &os, const Array &b);
--- modules/module-07/ex02/src/main.cpp
+++ modules/module-07/ex02/src/main.cpp
@@ -1,19 +1,8 @@
-#include	"../include/whatever.hpp"
+#include	"../include/Array.hpp"
 
 int     main()
 {
-	int a = 2;
-	int b = 3;
-	std::cout << "a = 2 , b = 3" << std::endl;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	return 0;
+	int *a = new int();
+	*a = 5;
+	std::cout << *a << std::endl;
 }
