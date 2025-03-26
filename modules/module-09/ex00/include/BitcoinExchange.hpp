9442c3187c21a129325d52daada4579c39635a15
natrijau <>
2025-03-26 15:44:23 +0100

    module 09 ex00 btc modified

--- modules/module-09/ex00/include/BitcoinExchange.hpp
+++ modules/module-09/ex00/include/BitcoinExchange.hpp
@@ -19,6 +19,7 @@ void	BitcoinExchange(char *arg)
 
     std::map<std::string, float> btcExchange;
     std::string line;
+	std::getline(file, line);
     while (std::getline(file, line))
 	{
 		size_t splitComma = line.find(',');
@@ -32,29 +33,29 @@ void	BitcoinExchange(char *arg)
 	}
 	file.close();
     for (std::map<std::string, float>::iterator it = btcExchange.begin(); it != btcExchange.end(); ++it)
-        std::cout << std::fixed << std::setprecision(2) << "Ligne " << it->first << ": " << it->second << std::endl;
+        std::cout  << "Ligne " << it->first << ": " << it->second << std::endl;
 
 
 
-	std::ifstream file(arg);
+	// std::ifstream file(arg);
     
-	/*TROW ERROR*/
-	//if (!file)
-		//return (1);
+	// /*TROW ERROR*/
+	// //if (!file)
+	// 	//return (1);
 
-    std::map<std::string, float> input;
-    std::string line;
-    while (std::getline(file, line))
-	{
-		size_t splitComma = line.find(',');
-		if (splitComma != std::string::npos)
-		{
-			char *endptr;
-			std::string date = line.substr(0, splitComma);
-			double		price = std::strtod(line.substr(splitComma + 1).c_str(), &endptr);
-			input[date] = price;
-		}
-	}
+    // std::map<std::string, float> input;
+    // std::string line;
+    // while (std::getline(file, line))
+	// {
+	// 	size_t splitComma = line.find(',');
+	// 	if (splitComma != std::string::npos)
+	// 	{
+	// 		char *endptr;
+	// 		std::string date = line.substr(0, splitComma);
+	// 		double		price = std::strtod(line.substr(splitComma + 1).c_str(), &endptr);
+	// 		input[date] = price;
+	// 	}
+	// }
 
 
 
